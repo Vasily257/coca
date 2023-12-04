a9bfe3bb91241321da16e5ec26f7923aefafd430
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-12-04 09:41:20 +0400

    feat: add colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['./assets/css/fonts.css', './assets/css/main.css'],
+  css: ['./assets/css/fonts.css', './assets/css/colors.css', './assets/css/main.css'],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <h1>Digitally forward creative</h1>
+  <h1 class="home__title">Digitally forward creative</h1>
 </template>
 
 <style lang="scss">
 .home {
   &__title {
     letter-spacing: -3px;
-    color: #1d1e25;
+    color: var(--neutral-dark, #1d1e25);
     font-size: 80px;
     font-weight: 700;
     font-feature-settings: 'calt' off;
