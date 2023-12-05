8965823afb75143ddf6e95c2b3b046f6a02bff42
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-12-05 09:17:57 +0400

    feat: add the base icon component

---
+++ components/base/BaseIcon.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+/** Типы пропсов */
+interface Props {
+  /** Путь к спрайту */
+  spritePath: string;
+  /** Название иконки */
+  iconName: string;
+  /** Скрыть ли кнопку от скринридера */
+  ariaHidden?: boolean;
+}
+
+/** Пропсы со значениями по умолчанию */
+const props = withDefaults(defineProps<Props>(), {
+  ariaHidden: true,
+});
+</script>
+
+<template>
+  <svg
+    class="icon"
+    :aria-hidden="ariaHidden"
+    role="img"
+  >
+    <use :xlink:href="`${spritePath}#${iconName}`" />
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+  flex-shrink: 0;
+  width: 16px;
+  height: 16px;
+}
+</style>
