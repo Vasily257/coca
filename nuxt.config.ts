602851a216ea406d2fd270a15a93b98f5f6747e9
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-12-05 11:19:36 +0400

    build: update eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['./assets/css/fonts.css', './assets/css/colors.css', './assets/css/main.css'],
-});
+  modules: [['@nuxtjs/eslint-module', { lintOnStart: false }]],
+  css: ['./assets/css/fonts.css', './assets/css/colors.css', './assets/css/main.css']
+})
