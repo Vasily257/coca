251293f2eabec40130e665c4864274b8419cf141
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-12-06 16:28:16 +0400

    feat: add the base link component

---
+++ components/base/BaseLink.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+/** Типы пропсов */
+interface Props {
+  /** Адрес, по которому нужно перейти */
+  to?: string;
+}
+
+/** Пропсы со значениями по умолчанию */
+const props = withDefaults(defineProps<Props>(), {
+  to: '/',
+});
+</script>
+
+<template>
+  <NuxtLink :to="to" class="link">
+    <slot></slot>
+  </NuxtLink>
+</template>
+
+<style lang="scss" scoped>
+.link {
+  text-decoration: none;
+  color: inherit;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus-visible {
+    outline: 2px solid #2f80ed;
+    outline-offset: 2px;
+  }
+}
+</style>
