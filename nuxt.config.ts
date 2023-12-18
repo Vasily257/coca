6d934443f327f221c68990aa27eb2b23e39596e2
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-12-18 15:25:18 +0400

    refactor(FSD): move a root component and global styles to the app folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['./assets/css/fonts.css', './assets/css/colors.css', './assets/css/main.css'],
 });
