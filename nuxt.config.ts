8c9fdd58d5adc2d4acd87e0f69654772310c581c
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-12-20 07:19:36 +0400

    build: add the image Nuxt plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@nuxt/image'],
 });
