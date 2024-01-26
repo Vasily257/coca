f962b37484dbbc5ead594ebd7fc0add7a94ab572
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2024-01-26 11:25:20 +0400

    refactor(home widget): use public API

--- src/pages/home/ui/HomePage.vue
+++ src/pages/home/ui/HomePage.vue
@@ -10,12 +10,14 @@
 </template>
 
 <script setup lang="ts">
-import { HomeData } from '@/widgets/home/data';
-import { HomeFeatures } from '@/widgets/home/features';
-import { HomeNews } from '@/widgets/home/news';
-import { HomePartners } from '@/widgets/home/partners';
-import { HomeRelations } from '@/widgets/home/relations';
-import { HomeReviews } from '@/widgets/home/reviews';
-import { HomeStrength } from '@/widgets/home/strength';
-import { HomeTop } from '@/widgets/home/top';
+import {
+  HomeData,
+  HomeFeatures,
+  HomeNews,
+  HomePartners,
+  HomeRelations,
+  HomeReviews,
+  HomeStrength,
+  HomeTop,
+} from '@/widgets/home';
 </script>
