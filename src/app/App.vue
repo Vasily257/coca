3814b060d1cf14803907a23814e5062466eb49a5
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2024-01-26 11:48:58 +0400

    feat: add styles for the home page

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,18 +1,43 @@
 <template>
   <TheHeader />
-  <main class="main">
+  <main :class="pageClass">
     <NuxtPage />
   </main>
   <TheFooter />
 </template>
 
 <script setup lang="ts">
+import { computed, useCssModule } from 'vue';
 import { TheFooter } from '@/widgets/the-footer';
 import { TheHeader } from '@/widgets/the-header';
+import { usePage } from '@/shared/lib';
+
+const { isHomePage } = usePage();
+
+/** CSS-классы */
+const classes = useCssModule();
+
+/** CSS-класс страницы */
+const pageClass = computed(() => ({
+  [classes.page]: true,
+  [classes.pageHome]: isHomePage,
+}));
 </script>
 
-<style lang="scss" scoped>
-.main {
-  min-height: calc(100vh);
+<style lang="scss" module>
+.page {
+  padding: 0 20px;
+
+  @media screen and (min-width: 768px) {
+    padding: 0 60px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    padding: 0 120px;
+  }
+
+  &Home {
+    margin-top: 29px;
+  }
 }
 </style>
